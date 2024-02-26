69fc205942d816cd09fffd75083a2f2c5b05388b
Lea Pateau <lea.pateau0@gmail.com>
2024-02-26 14:17:39 +0100

    Resized phone's form size Added eye images Changed images' names Modified icons

--- js/password.js
+++ js/password.js
@@ -6,12 +6,10 @@ function displayPassword() {
 	if (password.classList.contains('dots')) {
 		password.type = "text";
 		password.classList = 'visible';
-		// TODO : mettre les bonnes images (oeil barré)
-		eyeIcon.src = "images/bell.png";
+		eyeIcon.src = "images/closed_eye.png";
 	} else if (password.classList.contains('visible')) {
 		password.type = "password";
 		password.classList = 'dots';
-		// TODO : mettre les bonnes images (oeil non barré)
-		eyeIcon.src = "images/loupe.png";
+		eyeIcon.src = "images/opened_eye.png";
 	}
 }
